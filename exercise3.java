126a8d6a5f1e852122cff1780973eaf09d5a3469
anitha20003 <149779051+anitha20003@users.noreply.github.com>
2023-11-04 13:40:46 +0200

    Add files via upload

---
+++ exercise3.java
@@ -0,0 +1,23 @@
+public class exercise3 {// creating class
+ public static void main (String arges[]){
+  int classnumber=3;//initialisation
+  switch(classnumber){//to execute one statement from multiple one
+  case 1:System.out.println("esther");
+  break;
+  case 2:System.out.println("debor");
+  break;
+  case 3:System.out.println("ruth");
+  break;
+  case 4:System.out.println("gideo");
+  break;
+  case 5:System.out.println("aline");
+  break;
+  case 6:System.out.println("caline");
+  break;
+  case 7:System.out.println("lucky");
+  break;
+  default:System.out.println("unknown mame");
+
+  } 
+ } 
+}
