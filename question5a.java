f853b12d83b0240406bc219469f0a7e00ea2cd31
anitha20003 <149779051+anitha20003@users.noreply.github.com>
2023-11-04 13:43:37 +0200

    Add files via upload

---
+++ question5a.java
@@ -0,0 +1,16 @@
+public class question5a {// creating class
+public static void main (String[] args ) {
+int i=1;
+do{// do-while loop
+if(i==5){//condition
+i++;
+break;//switch statement
+    }
+    System.out.println(i);// result to printed
+    i++;
+}while(i<=10);
+
+}
+}
+ 
+
