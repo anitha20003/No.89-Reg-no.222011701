e25b7ebe8ddbc30df257f6c0bdedde426294eb9a
anitha20003 <149779051+anitha20003@users.noreply.github.com>
2023-11-04 13:51:44 +0200

    Add files via upload

---
+++ exercise2.java
@@ -0,0 +1,25 @@
+public class exercise2 {
+ public static void main(String args[]){
+    int age=35;
+    if(age>= 35)
+    {
+     System.out.println("you are very strong");
+
+    }
+    else if(age<35 && age>=25){
+        System.out.println("you are weak");
+    }
+    else if (age<15){
+        System.out.println("you are too weak");
+    }
+    else if (age <10){
+        System.out.println("you are too young");
+    }
+    else{
+    System.out.println("you are not allowed");
+    }
+    
+    
+    }
+ } 
+
