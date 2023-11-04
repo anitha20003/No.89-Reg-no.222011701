e7c5237a1775155154832c48a3d9cea9bef1a3d2
anitha20003 <149779051+anitha20003@users.noreply.github.com>
2023-11-04 14:00:24 +0200

    Add files via upload

---
+++ exercise1.java
@@ -0,0 +1,13 @@
+public class exercise1 {
+ public static void main(String args[]){
+    int a=10;
+    int b=15;
+    if(a>b){
+    System.out.println("a is greater");
+}
+else{
+    System.out.println("b is greater");
+}
+  
+}
+}
