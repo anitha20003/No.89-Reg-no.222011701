492d9b49ef4309c6ea7c802864b86182561e298d
anitha20003 <149779051+anitha20003@users.noreply.github.com>
2023-11-04 13:38:44 +0200

    Add files via upload

---
+++ question5c.java
@@ -0,0 +1,10 @@
+public class question5c {// class created
+    public static void main(String[]args){
+        for(int i=1;i<=10;i++){// condition,increment,initialisation
+            if(i==5){//excuted
+                break;//printing element
+            }
+            System.out.println(i);// result to printed
+        }
+    }
+}
