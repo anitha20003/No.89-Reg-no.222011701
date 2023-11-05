6d147e802508f91d4f1867124fc564ebd694518b
anitha20003 <149779051+anitha20003@users.noreply.github.com>
2023-11-05 10:10:25 +0200

    Add files via upload

---
+++ question3a.java
@@ -0,0 +1,9 @@
+public class question3a {// creating class
+ public static void main(String[]args){// create main method in program
+    int i=1;// initialization
+    do{// code to excuted
+        System.out.println(i);
+        i++;//increment
+    }while(i<=30);//condition
+ }   
+}
