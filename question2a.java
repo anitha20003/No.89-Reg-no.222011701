2f6d21681d642d27220dd6ce84e2a764edee1007
anitha20003 <149779051+anitha20003@users.noreply.github.com>
2023-11-05 10:06:27 +0200

    Add files via upload

---
+++ question2a.java
@@ -0,0 +1,10 @@
+public class question2a {// class created
+   public static void main(String[]args) {
+    for(int i=1;i<=5;i++){//initialisation,condition ,increment
+    for(int j=1;j<=i;j++){//initialisation,conditin,increment
+        System.out.println("@");//result to printed
+    }
+    System.out.println();
+    }
+   }
+}
