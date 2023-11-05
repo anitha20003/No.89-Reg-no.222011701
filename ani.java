d6cf7a407fe08c4412ddd05b396c9dc279b1bed0
anitha20003 <149779051+anitha20003@users.noreply.github.com>
2023-11-05 10:13:56 +0200

    Add files via upload

---
+++ ani.java
@@ -0,0 +1,8 @@
+public class ani {
+    public static void main(String args[]){  // mandatory java syntax
+        int x=10;  // initialisation
+        System.out.println(x++);  
+        System.out.println(++x); 
+        System.out.println(x--);  
+        System.out.println(--x);
+        }}  
