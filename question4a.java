1ab602a7d7c36ae3df9e97a872a192e553c23fbc
anitha20003 <149779051+anitha20003@users.noreply.github.com>
2023-11-05 10:11:04 +0200

    Add files via upload

---
+++ question4a.java
@@ -0,0 +1,9 @@
+public class question4a {//class to created
+    public static void main(String[]args){// mandatory java syntax
+        int i=1;// data type,initialisation
+        while(i<=10){// condition
+            System.out.println(i);// result to be printed
+            i++;
+        }
+    }
+}
